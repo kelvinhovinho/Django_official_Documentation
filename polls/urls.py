d911a6ac28f1953f726f15927029d300744af1f9
Akelvino <vinho892@gmail.com>
2025-01-21 15:26:12 +0300

    include django forms and used generic views

--- polls/urls.py
+++ polls/urls.py
@@ -4,8 +4,8 @@ from . import views
 app_name = "polls"
 
 urlpatterns = [
-    path("",views.index, name="Index"),
-    path("<int:question_id>/", views.detail, name="detail"),
-    path("<int:question_id>/result/", views.result, name="results"),
+    path("",views.IndexView.as_view(), name="Index"),
+    path("<int:pk>/", views.DetailView.as_view(), name="detail"),
+    path("<int:pk>/result/", views.ResultsView.as_view, name="results"),
     path("<int:question_id>/vote/", views.vote, name="vote")
 ]
